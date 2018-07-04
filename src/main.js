264a7de28ac622c94ee2c775058411b05507c504
Marlon Barcarol <marlon.barcarol@printed.com>
2018-07-04 12:41:46 +0100

    added reproduction

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+window.onerror = (message, source, lineno, colno, error) => {
+	// eslint-disable-next-line
+  console.log(message, source, lineno, colno, error);
+
+  // This is just to not throw the error and ignore it, as we are using console.
+  return true;
+};
+
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+throw new Error('Oops! An error occurred');
